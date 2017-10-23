8899bef4a3a08882220e757d00abb3c1e629c838
EnoxSoftware <enox.software@gmail.com>
2017-10-24 05:14:02 +0900

    update 1.0.4

--- Assets/FaceMaskExample/Texture2DFaceMaskExample/Texture2DFaceMaskExample.cs
+++ Assets/FaceMaskExample/Texture2DFaceMaskExample/Texture2DFaceMaskExample.cs
@@ -16,7 +16,7 @@ using UnityEngine.SceneManagement;
 namespace FaceMaskExample
 {
     /// <summary>
-    /// Texture2D face mask example.
+    /// Texture2D FaceMask Example
     /// </summary>
     [RequireComponent (typeof(TrackedMeshOverlay))]
     public class Texture2DFaceMaskExample : MonoBehaviour
@@ -98,9 +98,9 @@ namespace FaceMaskExample
         string haarcascade_frontalface_alt_xml_filepath;
 
         /// <summary>
-        /// The shape_predictor_68_face_landmarks_dat_filepath.
+        /// The sp_human_face_68_dat_filepath.
         /// </summary>
-        string shape_predictor_68_face_landmarks_dat_filepath;
+        string sp_human_face_68_dat_filepath;
 
         #if UNITY_WEBGL && !UNITY_EDITOR
         Stack<IEnumerator> coroutines = new Stack<IEnumerator> ();
@@ -120,7 +120,7 @@ namespace FaceMaskExample
             StartCoroutine (getFilePath_Coroutine);
             #else
             haarcascade_frontalface_alt_xml_filepath = OpenCVForUnity.Utils.getFilePath ("haarcascade_frontalface_alt.xml");
-            shape_predictor_68_face_landmarks_dat_filepath = DlibFaceLandmarkDetector.Utils.getFilePath ("shape_predictor_68_face_landmarks.dat");
+            sp_human_face_68_dat_filepath = DlibFaceLandmarkDetector.Utils.getFilePath ("sp_human_face_68.dat");
             Run ();
             #endif
         }
@@ -134,8 +134,8 @@ namespace FaceMaskExample
             coroutines.Push (getFilePathAsync_0_Coroutine);
             yield return StartCoroutine (getFilePathAsync_0_Coroutine);
 
-            var getFilePathAsync_1_Coroutine = DlibFaceLandmarkDetector.Utils.getFilePathAsync ("shape_predictor_68_face_landmarks.dat", (result) => {
-                shape_predictor_68_face_landmarks_dat_filepath = result;
+            var getFilePathAsync_1_Coroutine = DlibFaceLandmarkDetector.Utils.getFilePathAsync ("sp_human_face_68.dat", (result) => {
+                sp_human_face_68_dat_filepath = result;
             });
             coroutines.Push (getFilePathAsync_1_Coroutine);
             yield return StartCoroutine (getFilePathAsync_1_Coroutine);
@@ -185,7 +185,7 @@ namespace FaceMaskExample
             Debug.Log ("rgbaMat ToString " + rgbaMat.ToString ());
 
             if (faceLandmarkDetector == null)
-                faceLandmarkDetector = new FaceLandmarkDetector (shape_predictor_68_face_landmarks_dat_filepath);
+                faceLandmarkDetector = new FaceLandmarkDetector (sp_human_face_68_dat_filepath);
 
             FrontalFaceChecker frontalFaceChecker = new FrontalFaceChecker (width, height);
 
